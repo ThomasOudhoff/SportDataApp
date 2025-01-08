8483acf2abe61eff883ee7974077880f8f15498d
ThomasOudhoff <thomasoudhoff@live.nl>
2025-01-08 17:59:01 +0100

    NavBar afgemaakt.

--- src/Components/NavBar/NavBar.component.js
+++ src/Components/NavBar/NavBar.component.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import "./NavBar.component.css";
 
-export function NavBarComponent() {
+const NavBarComponent = () => {
+    const [dateTime, setDateTime] = useState("");
+
+    useEffect(() => {
+        const updateDateTime = () => {
+            const now = new Date();
+            const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+            const date = now.toLocaleDateString("nl-NL", options);
+            const time = now.getHours() + ":" + now.getMinutes();
+            setDateTime(`${date} - ${time}`);
+        };
+
+        updateDateTime(); // Initialiseer datum en tijd
+        const intervalId = setInterval(updateDateTime, 60000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className="navbar">
-            <ul>
-                <li>
-                    Home
-                </li>
-                <li>
-                    Teams
-                </li>
-                <li>
-                    Scores
-                </li>
-                <li>
-                    Login
-                </li>
-            </ul>
-        </div>
+            {/* Linkerzijde: Datum en Tijd */}
+            <div className="datetime">{dateTime}</div>
 
-    )
-}
+            {/* Rechterzijde: Navigatielinks */}
+            <div className="nav-links">
+                <a href="#home">Home</a>
+                <a href="#teams">Teams</a>
+                <a href="#scores">Scores</a>
+                <a href="#login">Login</a>
+            </div>
+        </div>
+    );
+};
+
 export default NavBarComponent;
